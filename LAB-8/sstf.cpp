5ede6d55c29e9879a83d8ef3b5eb9f982808d07c
Hardik Rana <hardikrana276@gmail.com>
2018-04-01 20:23:41 +0530

    changed sstf

--- LAB-8/sstf.cpp
+++ LAB-8/sstf.cpp
@@ -1,82 +1,52 @@
 #include<bits/stdc++.h>
-
-struct di
-{
-int num;
-int flag;  //process is taken or not
-};
+using namespace std;
 
 int main()
 {
- int i,j,sum=0,n,min1,loc,x,y;
- //struct disk d[20];
- int disk;
- //int ar[20],a[20];
 
- printf("enter number of location\n");
- scanf("%d",&n);
+    int head;
+    printf("enter head position\n");
+    cin>>head;
 
- struct di d[n];
- int ar[n],a[n];
-
- printf("enter position of head\n");
- scanf("%d",&disk);
-
- printf("enter elements of disk queue\n");
- for(i=0;i<n;i++)
- {
-  scanf("%d",&d[i].num);
-  d[i].flag=0;
- }
-
- for(i=0;i<n;i++)
- {
-   x=0; min1=0;loc=0;
-   for(j=0;j<n;j++)
-   {
-     if(d[j].flag==0)
-     {
-      if(x==0)
-      {
-       ar[j]=disk-d[j].num;
-       if(ar[j]<0)
-       {
-         ar[j]=d[j].num-disk;
-       }
-       min1=ar[j];
-       loc=j;
-       x++;
-      }
-     else
-     {
-       ar[j]=disk-d[j].num;
-       if(ar[j]<0)
-       {
-        ar[j]=d[j].num-disk;
-       }
-     }
-     if(min1>ar[j])
-      {
-         min1=ar[j];
-         loc=j;
-      }
-     }
-    }
-    d[loc].flag=1;
-    a[i]=d[loc].num-disk;
-    if(a[i]<0)
+    int n,i;
+    cout<<"enter total requets"<<endl;
+    cin>>n;
+    int queue1[n],done[n];
+    printf("enter requests\n");
+    for(i=0;i<n;i++)
     {
-     a[i]=disk-d[loc].num;
+        scanf("%d",&queue1[i]);
+        done[i]=0;
     }
 
-     disk=d[loc].num;
- }
+    printf("%d->",head);
+
+    int temp1=head;
+    int count1=0,j,sum=0;
+    while(count1!=n)
+    {
+    int temp=100000;
+    int k=-1;
+    for(j=0;j<n;j++)
+    {
+        if(temp1!=queue1[j] && done[j]==0)
+        {
+        if(temp>abs(temp1-queue1[j]))
+        {
+           temp = abs(temp1-queue1[j]);
+           k = j;
+        }
+        }
+    }
+    sum+=temp;
+    printf("%d->",queue1[k]);
+    count1++;
+    temp1=queue1[k];
+    done[k]=1;
+    //cout<<temp1<<endl;
+    }
+    cout<<endl;
+    cout<<"total head movement is="<<sum<<endl;
 
 
- for(i=0;i<n;i++)
- {
- sum=sum+a[i];
- }
- printf("\nmovement of total cylinders %d",sum);
- return 0;
 }
